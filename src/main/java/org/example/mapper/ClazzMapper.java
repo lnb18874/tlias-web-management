cf2055ac72210bd405d7a2ef0bf1f2506c13ab09
lnb18874 <linniubi925@gmail.com>
2026-09-06 20:49:58 +0800

    删了一个import

--- src/main/java/org/example/mapper/ClazzMapper.java
+++ src/main/java/org/example/mapper/ClazzMapper.java
@@ -1,7 +1,6 @@
 package org.example.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.example.pojo.Clazz;
 import org.example.pojo.ClazzQueryParam;
 
